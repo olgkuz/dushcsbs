3d9c1941feafd9a2ae4a08dadf2c5e111e353e0a
olgKuz <kuznecovao@list.ru>
2025-07-31 19:31:10 +0300

    31/07/25

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  // Используем NestExpressApplication, чтобы подключить статические файлы
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   // Настройка CORS
   app.enableCors({
-    origin: 'http://localhost:4200', 
-    
+    origin: 'http://localhost:4200',
+  });
+
+  // Статические файлы из папки "public"
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/public/', // доступно по адресу http://localhost:3000/public/filename.jpg
   });
 
   // Глобальная валидация
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Автоматически удаляет лишние поля
-      forbidNonWhitelisted: true, // Бросает ошибку при наличии лишних полей
-      transform: true // Автоматическое преобразование типов
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
     })
   );
 
-  // Запуск приложения
+  // Запуск
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
